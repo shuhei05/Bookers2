80619385bdde11771a8212825354232159c5b0fe
shuhei05 <qoo0228y@gmail.com>
2021-04-10 03:52:30 +0000

    delete,edit終了

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -22,16 +22,17 @@ def create
   redirect_to book_path(@book.id)
 end
 
-def
-  edit
+def edit
+  @book = Book.find(params[:id])
 end
 
-def
-  updated
+def updated
 end
 
-def
-  destroy
+def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
 end
 
 private
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,13 +28,14 @@ class UsersController < ApplicationController
 
   end
 
-  def
-  destroy
+  def destroy
+
+
   end
 
   private
 def user_params
-   params.require(:user).permit(:name, :introduction, :profile_image,)
+   params.require(:user).permit(:name, :introduction, :profile_image)
 end
 
 end
