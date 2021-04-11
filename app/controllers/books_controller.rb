8d1435e113be31e1e07e83a0c4140b4e5fd0ec10
shuhei05 <qoo0228y@gmail.com>
2021-04-11 06:03:12 +0000

    チェック53まで終了

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -23,7 +23,7 @@ def create
   @book.user_id = current_user.id
   if
      @book.save
-     redirect_to book_path(@book.id)
+     redirect_to book_path(@book.id),notice:'You have created book successfully.'
   else
      @books = Book.all
      @user = current_user
@@ -39,7 +39,7 @@ def update
     @book = Book.find(params[:id])
     if
         @book.update(book_params)
-        redirect_to book_path(@book.id)
+        redirect_to book_path(@book.id),notice:'You have updated book successfully.'
     else
         render :edit
     end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,7 +29,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     if
       @user.update(user_params)
-      redirect_to user_path(@user.id)
+      redirect_to user_path(@user.id),notice:'You have updated user successfully.'
     else
       render :edit
     end
